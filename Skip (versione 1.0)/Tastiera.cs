5f446847d99f5ab8ed732b7227ca882a257d474a
Andrea Parodi <andreparo63@hotmail.it>
2017-03-28 01:01:16 +0200

    Aggiunte le primissime bozze delle classi "Tastiera" e "Tasto" (Tastiera.cs e Tasto.cs)

---
+++ Skip (versione 1.0)/Tastiera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Skip
+{
+    internal class Tastiera
+    {
+        public int numRighe;
+        public int numColonne;
+        //in teoria questo numTastiere non serve
+        //si può creare direttamente una lista di tastiere dentro MainForm.cs
+        public int numTastiere;
+        //pensavo di creare una struttura (matrice array o lista) di tasti che contenga i tasti della specifica tastiera
+        public Tasto[,] matriceTasti;
+        public List<Tasto> listaTasti;
+        public Tastiera()
+        {
+            this.numColonne = 0;
+            this.numColonne = 0;
+            this.numTastiere = 0;
+        }
+        public Tastiera(int righe, int colonne)
+        {
+            this.numColonne = righe;
+            this.numColonne = colonne;
+            this.numTastiere = 0;
+        }
+        //anche questo costruttore si può togliere
+        public Tastiera(int tastiere)
+        {
+            this.numColonne = 0;
+            this.numColonne = 0;
+            this.numTastiere = tastiere;
+        }
+        public void aggiungiTasto(Tasto tasto)
+        {
+            this.listaTasti.Add(tasto);
+        }
+    }
+}
---
+++ Skip (versione 1.0)/Tasto.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Skip
+{
+    internal class Tasto
+    {
+        public int relPosX;
+        public int relPosY;
+        public string contenuto;
+
+        public Tasto()
+        {
+            this.relPosX = 0;
+            this.relPosY = 0;
+            this.contenuto = "";
+        }
+        public Tasto(int x, int y)
+        {
+            this.relPosX = x;
+            this.relPosX = y;
+            this.contenuto = "";
+        }
+        public Tasto(int x, int y, string tasto)
+        {
+            this.relPosX = x;
+            this.relPosX = y;
+            this.contenuto = tasto;
+        }
+    }
+}
