40d3143b104e2d7273a7d1c424ffe9aac4e1b505
“Manuele <manus93@live.it>
2017-03-28 17:11:44 +0200

    Progressi per classi Tastiera e Tasto, prima implementazione del metodo per disegnare la tastiera

--- Skip (versione 1.0)/Tastiera.cs
+++ Skip (versione 1.0)/Tastiera.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Skip
 {
@@ -6,34 +8,76 @@ namespace Skip
     {
         public int numRighe;
         public int numColonne;
-        //in teoria questo numTastiere non serve
-        //si può creare direttamente una lista di tastiere dentro MainForm.cs
-        public int numTastiere;
         //pensavo di creare una struttura (matrice array o lista) di tasti che contenga i tasti della specifica tastiera
         public Tasto[,] matriceTasti;
         public List<Tasto> listaTasti;
+        public int origineX, origineY; // la posizione assoluta della tastiera nella form
+
+        public Font font;
         public Tastiera()
         {
             this.numColonne = 0;
-            this.numColonne = 0;
-            this.numTastiere = 0;
+            this.numRighe = 0;
+            matriceTasti = new Tasto[9, 18]; // 9 x 18 è la tastiera di default
+            origineX = 0;
+            origineY = 0;
         }
-        public Tastiera(int righe, int colonne)
+        public Tastiera(int righe, int colonne, Font f)
         {
-            this.numColonne = righe;
+            this.numRighe = righe;
             this.numColonne = colonne;
-            this.numTastiere = 0;
-        }
-        //anche questo costruttore si può togliere
-        public Tastiera(int tastiere)
-        {
-            this.numColonne = 0;
-            this.numColonne = 0;
-            this.numTastiere = tastiere;
+            matriceTasti = new Tasto[righe, colonne];
+            origineX = 10; // valori di prova
+            origineY = 40;
+            font = f;
         }
         public void aggiungiTasto(Tasto tasto)
         {
             this.listaTasti.Add(tasto);
+        }
+
+        public void disegnaTastiera(Graphics graphics, Color orth_fg_col, Color orth_bg_col, Color rorth_fg_col, Color rorth_bg_col, Color corth_fg_col, Color corth_bg_col, Color other_fg_col, Color other_bg_col, Color indic_col, Color presel0_col, Color presel1_col)
+        {
+            SizeF dimensioneTesto; // dimensione del testo da scrivere sul tasto
+            PointF posizioneTesto = new PointF(); // dove scrivere il testo sul tasto
+            Color coloreSfondo, coloreTesto; // colori di testo e sfondo del tasto
+
+            for (int i = 0; i < numRighe; i++) // disegnamo i tasti uno per uno
+            {
+                for (int j = 0; j < numColonne; j++)
+                { // TODO: bisogna gestire il fatto che un tasto può essere premuto (dunque avrà un colore diverso)
+                    // e eventualmente bisognerà disegnare il suo completamento
+                    dimensioneTesto = graphics.MeasureString(matriceTasti[i, j].contenuto, font);
+                    switch (matriceTasti[i, j].tipo)
+                    {
+                        case Tasto.TipoTasto.Ortogonale:
+                            coloreSfondo = orth_bg_col;
+                            coloreTesto = orth_fg_col;
+                            break;
+                        case Tasto.TipoTasto.Riga:
+                            coloreSfondo = rorth_bg_col;
+                            coloreTesto = rorth_fg_col;
+                            break;
+                        case Tasto.TipoTasto.Colonna:
+                            coloreSfondo = corth_bg_col;
+                            coloreTesto = corth_fg_col;
+                            break;
+                        case Tasto.TipoTasto.Altro:
+                            coloreSfondo = other_bg_col;
+                            coloreTesto = other_fg_col;
+                            break;
+                        default:
+                            coloreSfondo = Color.White;
+                            coloreTesto = Color.Black;
+                            break;
+                    }
+                    graphics.FillPath(new SolidBrush(coloreSfondo), matriceTasti[i, j].perimetro); // riempie lo sfondo del tasto
+                    posizioneTesto.X = matriceTasti[i,j].xCentro - (dimensioneTesto.Width / 2);
+                    posizioneTesto.Y = matriceTasti[i, j].yCentro - (dimensioneTesto.Height / 2);
+                    graphics.DrawString(matriceTasti[i, j].contenuto, font, new SolidBrush(coloreTesto), posizioneTesto);
+                    graphics.DrawPath(new Pen(Color.Black, 2), matriceTasti[i, j].perimetro); // disegna il contorno del tasto
+                }
+            }
         }
     }
 }
--- Skip (versione 1.0)/Tasto.cs
+++ Skip (versione 1.0)/Tasto.cs
@@ -1,39 +1,99 @@
 ﻿
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
 namespace Skip
 {
     internal class Tasto
     {
-        public int relPosX;
-        public int relPosY;
+        public int relPosX; // indici che tengono conto della posizione del tasto nella tastiera
+        public int relPosY; // (esempio: il tasto in alto a sx avrà relPosX == 0 e relPosY == 0)
         //per ora le dimensioni dei tasti sono settate tutte a zero: bisogna fare i casi (altezza dovrebbe essere uguale per tutti) 
         //ma per esempio per maiusc/spazio avrò dimensione orizzontale diversa
-        public int xDimension;
-        public int yDimension;
+        public static int xDimension; // static così vi si può accedere senza necessariamente istanziarne un oggetto
+        public static int yDimension;
         public string contenuto;
+        public List<Tasto> completamento;
+        public enum Stato { Attivo, Premuto }
+        public Stato stato;
+        public enum TipoTasto { Ortogonale, Riga, Colonna, Altro, Completamento }
+        public TipoTasto tipo;
+        public GraphicsPath perimetro;
+        public Point[] vertici;
+        public int xCentro, yCentro;
 
         public Tasto()
         {
             this.relPosX = 0;
             this.relPosY = 0;
-            this.xDimension = 0;
-            this.yDimension = 0;
+            xDimension = 0;
+            yDimension = 0;
             this.contenuto = "";
+            completamento = new List<Tasto>();
+            stato = Stato.Attivo;
         }
         public Tasto(int x, int y)
         {
             this.relPosX = x;
-            this.relPosX = y;
-            this.xDimension = 0;
-            this.yDimension = 0;
+            this.relPosY = y;
+            xDimension = 0;
+            yDimension = 0;
             this.contenuto = "";
+            completamento = new List<Tasto>();
+            stato = Stato.Attivo;
         }
         public Tasto(int x, int y, string tasto)
         {
             this.relPosX = x;
-            this.relPosX = y;
-            this.xDimension = 0;
-            this.yDimension = 0;
+            this.relPosY = y;
+            xDimension = 30; // sono prove, più avanti vedremo di aggiustare questi valori
+            yDimension = 15;
             this.contenuto = tasto;
+            completamento = new List<Tasto>();
+            stato = Stato.Attivo;
+            vertici = new Point[4];
+            perimetro = new GraphicsPath();
+        }
+        public Tasto(int x, int y, string tasto, int xCentro, int yCentro)
+        {
+            this.relPosX = x;
+            this.relPosY = y;
+            xDimension = 30; // sono prove, più avanti vedremo di aggiustare questi valori
+            yDimension = 15;
+            this.contenuto = tasto;
+            completamento = new List<Tasto>();
+            stato = Stato.Attivo;
+            vertici = new Point[4];
+            perimetro = new GraphicsPath();
+            vertici[0] = new Point(xCentro - xDimension / 2, yCentro - yDimension / 2); // vertice in alto a sx
+            vertici[1] = new Point(xCentro + xDimension / 2, yCentro - yDimension / 2); // vertice in alto a dx
+            vertici[2] = new Point(xCentro + xDimension / 2, yCentro + yDimension / 2); // vertice in basso a dx
+            vertici[3] = new Point(xCentro - xDimension / 2, yCentro + yDimension / 2); // vertice in basso a sx
+            perimetro.AddLine(vertici[0], vertici[1]);
+            perimetro.AddLine(vertici[1], vertici[2]);
+            perimetro.AddLine(vertici[2], vertici[3]);
+            perimetro.AddLine(vertici[3], vertici[0]);
+            this.xCentro = xCentro;
+            this.yCentro = yCentro;
+        }
+        // aggiunge a questo tasto il completamento da visualizzare quando è selezionato
+        public void aggiungiCompletamento(string[] tasti)
+        {
+            // l'ordine dei tasti nel completamento sarà il seguente:
+            // 1    2   3
+            // 4    5   6
+            // 7    8   9
+            completamento.Add(new Tasto(relPosX - 1, relPosY - 1, tasti[0])); // tasto 1
+            completamento.Add(new Tasto(relPosX - 1, relPosY, tasti[1])); // tasto 2
+            completamento.Add(new Tasto(relPosX - 1, relPosY + 1, tasti[2])); // tasto 3
+            completamento.Add(new Tasto(relPosX, relPosY - 1, tasti[3])); // tasto 4
+            completamento.Add(new Tasto(relPosX, relPosY, tasti[4])); // tasto 5
+            completamento.Add(new Tasto(relPosX, relPosY + 1, tasti[5])); // tasto 6
+            completamento.Add(new Tasto(relPosX + 1, relPosY - 1, tasti[6])); // tasto 7
+            completamento.Add(new Tasto(relPosX + 1, relPosY, tasti[7])); // tasto 8
+            completamento.Add(new Tasto(relPosX + 1, relPosY + 1, tasti[8])); // tasto 9
+            // probabilmente si può fare in modo più furbo, per adesso lo lascio così
         }
     }
 }
